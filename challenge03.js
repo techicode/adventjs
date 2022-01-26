fddabcf7f432c006bdda3a48450baaddd57e1806
techicode <techicode@outlook.com>
2022-01-26 19:09:08 -0300

    comments on challenge 03

--- challenge03.js
+++ challenge03.js
@@ -1,18 +1,30 @@
 const isValid = (letter) => {
+
+  // contador para ver si existe el mismo numero de ( y de )
   let counter = 0;
+
+  // for in para poder acceder al siguiente elemento
   for (const key in letter) {
     if (letter[key] === '(' || letter[key] === ")") {
       counter++;
+
+      // segundo argumento de includes es desde donde empieza a comprobar
       if (letter[key] === '(' && !letter.includes(")", key))
         return false;
+
+      // si el caracter siguiente es un ), significa que el parentesis esta vacio
       if (letter[Number.parseInt(key) + 1] === ")") {
         return false;
       }
     }
+
+    // dentro del recorrido, estos caracteres estan prohibidos por ende returna falso
     if (letter[key] === '[' || letter[key] === '{') {
       return false;
     }
   }
+
+  // si el contador devuelve numero impar es que hay un paretensis sin cerrar o abrir
   if (counter % 2 !== 0) {
     return false;
   }
